8134611b92ed0542537a07a70b66dc1012c1cb98
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-04-24 16:31:05 -0700

    Adding best C and sigma for the dataset 3

--- mlclass-ex6-005/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-005/mlclass-ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0;
+sigma = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+errors = [];
+C_values = [];
+sigma_values = [];
 
+possible_values = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+C_index = 1;
+while C_index <= size(possible_values, 2),
+  sigma_index = 1;
 
+  while sigma_index <= size(possible_values, 2),
 
+    C_values(end + 1) = possible_values(C_index);
+    sigma_values(end + 1) = possible_values(sigma_index);
 
+    model = svmTrain(X, y, C_values(end), @(x1, x2) gaussianKernel(x1, x2, sigma_values(end)));
+    predictions = svmPredict(model, Xval);
+    errors(end + 1) = mean(double(predictions ~= yval));
+
+    sigma_index++;
+  end;
+
+  C_index++;
+end;
+
+[min_value, min_index] = min(errors);
+C = C_values(min_index);
+sigma = sigma_values(min_index);
 
 % =========================================================================
 
