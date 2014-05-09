1199a10f2dd16775205d92384918016c094353fe
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-05-09 09:55:21 -0700

    Adding gradients

--- mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
@@ -42,6 +42,9 @@ Theta_grad = zeros(size(Theta));
 
 J = 1 / 2 * sum(sum((X * Theta' .- Y) .^ 2 .* R));
 
+X_grad = ((X * Theta' .- Y) .* R) * Theta;
+Theta_grad = ((X * Theta' .- Y) .* R)' * X;
+
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
