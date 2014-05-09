7c051bd64c2cf7655b4d6de967ee715a1997a8be
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-05-09 10:19:52 -0700

    Adding regulation to gradients

--- mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8-005/mlclass-ex8/cofiCostFunc.m
@@ -44,8 +44,8 @@ regulation_theta = lambda / 2 * sum(sum(Theta .^ 2));
 regulation_x = lambda / 2 * sum(sum(X .^ 2));
 J = 1 / 2 * sum(sum((X * Theta' .- Y) .^ 2 .* R)) + regulation_theta + regulation_x;
 
-X_grad = ((X * Theta' .- Y) .* R) * Theta;
-Theta_grad = ((X * Theta' .- Y) .* R)' * X;
+X_grad = ((X * Theta' .- Y) .* R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' .- Y) .* R)' * X + lambda * Theta;
 
 % =============================================================
 
