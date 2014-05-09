8b8fe4592eb66746c267e588c4d80b565ec9288b
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-05-09 09:42:36 -0700

    Adding threshold selection

--- mlclass-ex8-005/mlclass-ex8/selectThreshold.m
+++ mlclass-ex8-005/mlclass-ex8/selectThreshold.m
@@ -23,18 +23,17 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    above_threshold = pval < epsilon;
 
+    true_positives = sum(above_threshold == 1 & yval == 1);
+    false_positives = sum(above_threshold == 1 & yval == 0);
+    false_negatives = sum(above_threshold == 0 & yval == 1);
 
+    precision = true_positives / (true_positives + false_positives);
+    recall = true_positives / (true_positives + false_negatives);
 
-
-
-
-
-
-
-
-
-
+    F1 = 2 * precision * recall / (precision + recall);
+    
     % =============================================================
 
     if F1 > bestF1
